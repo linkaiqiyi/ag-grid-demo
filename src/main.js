ebe09b60b5904a05149e2e276f9bbf8707726a27
linkai <87741822+linkaiqiyi@users.noreply.github.com>
2022-02-17 17:13:50 +0800

    init init project

--- src/main.js
+++ src/main.js
@@ -1,26 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import App2 from './App2.vue'
-import App3 from './App3.vue'
-import App4 from './App4.vue'
-import App5 from './App5.vue'
-import App6 from './App6.vue'
-import App7 from './App7.vue'
-import App9 from './App9.vue'
-import App11 from './App11.vue'
-import App12 from './App12.vue'
-import App13 from './App13.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import App2 from "./App2.vue";
+import App3 from "./App3.vue";
+import App4 from "./App4.vue";
+import App5 from "./App5.vue";
+import App6 from "./App6.vue";
+import App7 from "./App7.vue";
+import App9 from "./App9.vue";
+import App11 from "./App11.vue";
+import App12 from "./App12.vue";
+import App13 from "./App13.vue";
 
-import VueCompositionAPI from '@vue/composition-api'
+import { LicenseManager } from "ag-grid-enterprise";
+import VueCompositionAPI from "@vue/composition-api";
 
-Vue.use(VueCompositionAPI)
-Vue.config.productionTip = false
+Vue.use(VueCompositionAPI);
+Vue.config.productionTip = false;
 
-import { LicenseManager } from 'ag-grid-enterprise'
-
-LicenseManager.prototype.isDisplayWatermark = () => false
-LicenseManager.prototype.outputMissingLicenseKey = () => {}
+LicenseManager.prototype.isDisplayWatermark = () => false;
+LicenseManager.prototype.outputMissingLicenseKey = () => {};
 
 new Vue({
-  render: h => h(App13),
-}).$mount('#app')
+  render: (h) => h(App13)
+}).$mount("#app");
